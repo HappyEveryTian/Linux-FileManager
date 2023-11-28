b8dcf3a59c8e5f573205f288741ca849bff8f657
LostInTemple <131500984+HappyEveryTian@users.noreply.github.com>
2023-11-28 09:56:54 +0800

    Update fileos2.py

--- 旧的版本/fileos2.py
+++ 旧的版本/fileos2.py
@@ -781,6 +781,7 @@ class DisposeTab2(QtWidgets.QTabWidget):
                         os.makedirs(file_path, exist_ok=True)
                         #将新创建的文件夹名称添加到左侧的文件列表中
                         file1 = os.listdir(dir1)
+                        ui1.listWidget1.clear()
                         for i in file1:
                             file_path = os.path.join(dir1, i)
                             item = QtWidgets.QListWidgetItem()
@@ -1060,6 +1061,7 @@ class DisposeTab3(QtWidgets.QTabWidget):
                         with open(file_path, 'w') as file:
                             pass
                         file2 = os.listdir(dir2)
+                        ui2.listWidget2.clear()
                         for i in file2:
                             file_path = os.path.join(dir2, i)
                             item = QtWidgets.QListWidgetItem()
